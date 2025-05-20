57f6604ab50f28b110c6d1ddad8db645aa00e75b
cheptoo6377 <cheptoodorothy69@gmail.com>
2025-05-20 12:02:12 +0300

    new additions

--- consuming_api/restcountries.py
+++ consuming_api/restcountries.py
@@ -3,29 +3,28 @@ from flask import Flask, jsonify
 import requests
 
 app = Flask(__name__)
-API_URL = ' https://restfulcountries.com/api/v1'
+API_URL = 'https://restcountries.com/v3.1'
 
-@app.route('/countries',methods=['GET'])
+@app.route('/countries', methods=['GET'])
+
 def get_countries():
-    response = requests.get(f"{API_URL}/countries")
+    response = requests.get(f"https://restcountries.com/v3.1/countries")
     if response.status_code != 200:
         return jsonify({"error": "no countries found"}), 404
-    country_data = response.json()
-    return jsonify(country_data)
+    return jsonify(response.json())
 
 
-@app.route('/countries/<string:name>',methods=['GET'])
-def get_country(name):
-    response = requests.get(f"{API_URL}/name/{name}")
+
+
+
+@app.route('/countries/<country_name>', methods=['GET'])
+def get_country_by_name(country_name):
+    response = requests.get(f"https://restcountries.com/v3.1/name/{country_name}")
     if response.status_code != 200:
-        return jsonify({"error": "no country name found"}), 404
-    country_data = response.json()
-    return jsonify(country_data)
+        return jsonify({"error": "no country found"}), 404
+    return jsonify(response.json())
 
-
-
-
-
+    
 
 
 
